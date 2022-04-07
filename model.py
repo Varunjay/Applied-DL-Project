132d86d6518a833080ceea3b15e926fb2310feaa
rishabh-rustogi <rishabh.rustogi@gmail.com>
2022-04-07 17:47:51 -0400

    Changed comment in Model.py

--- model.py
+++ model.py
@@ -20,7 +20,7 @@ class Model:
     """
     def __init__(self, loc):
         """
-        Creates the model and laod the weight
+        Loads the model
         """
         bert_preprocess = hub.KerasLayer("https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3")
         bert_encoder = hub.KerasLayer("https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/4")
